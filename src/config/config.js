990f28be3a22746ed916f37564ca6710f58b37cd
Sarah Audina <sarahaudinar@gmail.com>
2022-05-31 17:12:15 +0700

    env update

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports  = {
     port: process.env.PORT || 1234,
     ip: process.env.HOST || '0.0.0.0',
     mongo: {
-      uri: process.env.MONGO_URL || 'mongodb://localhost:27017/calory'
+      uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/calory'
     },
     jwtSecret: process.env.JWT_SECRET || 'jkl!±@£!@ghj1237'
 };
